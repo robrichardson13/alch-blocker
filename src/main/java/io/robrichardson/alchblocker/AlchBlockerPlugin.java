6560e22a70fb56d92a64bb5db1d2f902a6043931
Rob Richardson <rob@robrichardson.io>
2022-10-03 18:19:38 -0700

    - Added shutdown method - Swapped text contains to text remove tags and then compare

--- src/main/java/io/robrichardson/alchblocker/AlchBlockerPlugin.java
+++ src/main/java/io/robrichardson/alchblocker/AlchBlockerPlugin.java
@@ -7,6 +7,7 @@ import net.runelite.api.*;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemManager;
@@ -28,12 +29,21 @@ public class AlchBlockerPlugin extends Plugin
 	private Client client;
 
 	@Inject
+	private ClientThread clientThread;
+
+	@Inject
 	private AlchBlockerConfig config;
 
 	@Inject
 	private ItemManager itemManager;
 
 	List<Integer> hiddenItems = new ArrayList<>();
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		clientThread.invoke(this::showBlockedItems);
+	}
 
 	@Provides
 	AlchBlockerConfig provideConfig(ConfigManager configManager)
@@ -44,7 +54,8 @@ public class AlchBlockerPlugin extends Plugin
 	@Subscribe()
 	public void onMenuOptionClicked(MenuOptionClicked event)
 	{
-		if(Objects.equals(event.getMenuOption(), "Cast") && (event.getMenuTarget().contains("High Level Alchemy") || event.getMenuTarget().contains("Low Level Alchemy"))) {
+		String menuTarget = Text.removeTags(event.getMenuTarget());
+		if(Objects.equals(event.getMenuOption(), "Cast") && (Objects.equals(menuTarget, "High Level Alchemy") || Objects.equals(menuTarget, "Low Level Alchemy"))) {
 			hideBlockedItems();
 		} else {
 			showBlockedItems();
@@ -60,7 +71,8 @@ public class AlchBlockerPlugin extends Plugin
 		for (Widget inventoryItem : Objects.requireNonNull(inventory.getChildren())) {
 			List<String> blockedItems = Text.fromCSV(config.blockedItems());
 			for(String blockedItem : blockedItems) {
-				if(inventoryItem.getName().toLowerCase().contains(blockedItem.toLowerCase())) {
+				String itemName = Text.removeTags(inventoryItem.getName()).toLowerCase();
+				if(itemName.equals(blockedItem.toLowerCase())) {
 					inventoryItem.setHidden(true);
 					hiddenItems.add(inventoryItem.getId());
 				}
