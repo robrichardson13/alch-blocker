18cdc06044630543eec6a2b38beffbedc5064754
RedSparr0w <RedSparr0w@users.noreply.github.com>
2023-07-21 22:19:49 +1200

    Block alching with explorer ring, opacity instead of hiding

--- src/main/java/io/robrichardson/alchblocker/AlchBlockerPlugin.java
+++ src/main/java/io/robrichardson/alchblocker/AlchBlockerPlugin.java
@@ -6,13 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.events.ScriptPostFired;
+import net.runelite.api.events.WidgetLoaded;
 import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetID;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
-import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.util.Text;
@@ -35,9 +36,6 @@ public class AlchBlockerPlugin extends Plugin
 
 	@Inject
 	private AlchBlockerConfig config;
-
-	@Inject
-	private ItemManager itemManager;
 
 	Set<String> itemList = new HashSet<>();
 	Set<Integer> hiddenItems = new HashSet<>();
@@ -70,7 +68,11 @@ public class AlchBlockerPlugin extends Plugin
 	@Subscribe()
 	public void onMenuOptionClicked(MenuOptionClicked event) {
 		String menuTarget = Text.removeTags(event.getMenuTarget());
-		isAlching = Objects.equals(event.getMenuOption(), "Cast") && (Objects.equals(menuTarget, "High Level Alchemy") || Objects.equals(menuTarget, "Low Level Alchemy"));
+		isAlching = event.getMenuOption().contains("Alchemy") || (event.getMenuOption().equals("Cast") && menuTarget.contains("Level Alchemy"));
+		// If item in our list of blocked items, don't allow the action
+		if (isAlching && hiddenItems.contains(event.getItemId())) {
+			event.consume();
+		}
 		if(!isAlching) {
 			showBlockedItems();
 		}
@@ -83,25 +85,35 @@ public class AlchBlockerPlugin extends Plugin
 		}
 	}
 
+	@Subscribe
+	private void onWidgetLoaded(WidgetLoaded event) {
+		if (event.getGroupId() == WidgetID.EXPLORERS_RING_ALCH_GROUP_ID) {
+			hideBlockedItems();
+		}
+	}
+
 	private void hideBlockedItems() {
-		Widget inventory = client.getWidget(WidgetInfo.INVENTORY);
+		Widget inventory = client.getWidget(WidgetInfo.EXPLORERS_RING_ALCH_INVENTORY);
 		if (inventory == null) {
-			return;
+			inventory = client.getWidget(WidgetInfo.INVENTORY);
+			if (inventory == null) {
+				return;
+			}
 		}
 
 		for (Widget inventoryItem : Objects.requireNonNull(inventory.getChildren())) {
 			String itemName = Text.removeTags(inventoryItem.getName()).toLowerCase();
 
-			boolean shouldHide = !config.blacklist();
+			boolean isBlacklist = !config.blacklist();
 			for(String blockedItem : itemList) {
 				if(WildcardMatcher.matches(blockedItem, itemName)) {
-					shouldHide = config.blacklist();
+					isBlacklist = config.blacklist();
 					break;
 				}
 			}
 
-			if(shouldHide) {
-				inventoryItem.setHidden(true);
+			if(isBlacklist) {
+				inventoryItem.setOpacity(220);
 				hiddenItems.add(inventoryItem.getItemId());
 			}
 		}
@@ -112,14 +124,19 @@ public class AlchBlockerPlugin extends Plugin
 			return;
 		}
 
-		Widget inventory = client.getWidget(WidgetInfo.INVENTORY);
+		Widget inventory = client.getWidget(WidgetInfo.EXPLORERS_RING_ALCH_INVENTORY);
+		if (inventory != null) {
+			// We don't need to show items again if we are in the explorer ring widget
+			return;
+		}
+		inventory = client.getWidget(WidgetInfo.INVENTORY);
 		if (inventory == null) {
 			return;
 		}
 
 		for (Widget inventoryItem : Objects.requireNonNull(inventory.getChildren())) {
 			if(hiddenItems.contains(inventoryItem.getItemId())) {
-				inventoryItem.setHidden(false);
+				inventoryItem.setOpacity(0);
 			}
 		}
 
